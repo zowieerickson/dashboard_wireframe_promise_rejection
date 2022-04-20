386ab532666181540a6eea4effca9438449a67a7
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-20 14:28:49 -0400

    calling unsplash api to generate random nature background image

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hi')
+fetch("https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=RYD8BDXw36XxEEc0URIbaQgqlXvKGmM7gRqmB9fzG3E")
+    .then(rsp => rsp.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.full})`
+    })
