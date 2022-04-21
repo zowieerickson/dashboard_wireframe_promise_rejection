de372683249d586fd2ee52c52d94e8db0ad7dca4
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-21 15:16:02 -0400

    added image's author through API

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 fetch("https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=RYD8BDXw36XxEEc0URIbaQgqlXvKGmM7gRqmB9fzG3E")
     .then(rsp => rsp.json())
     .then(data => {
+        document.querySelector("#author").textContent = data.user.name
         document.body.style.backgroundImage = `url(${data.urls.full})`
     })
