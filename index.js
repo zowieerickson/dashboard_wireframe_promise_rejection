d5afc6218f3232b571378b3d11b5935e6576fda5
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-22 11:45:48 -0400

    handling promise rejection with default image and default author

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ fetch("https://api.unsplash.com/photos/random?orientation=landscape&query=nature
         document.querySelector("#author").textContent = data.user.name
         document.body.style.backgroundImage = `url(${data.urls.full})`
     })
+    .catch(err => {
+        console.log(err)
+
+        const defaultImg = 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?crop=entropy&cs=srgb&fm=jpg&ixid=MnwzMjEyOTV8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTA2NDE5Mjg&ixlib=rb-1.2.1&q=85';
+        const defaultAuthor = 'Kalen Emsley';
+
+        document.body.style.backgroundImage = `url(${defaultImg})`;
+        document.querySelector("#author").textContent = defaultAuthor;
+    })
