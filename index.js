c27c01feb9ec6152296268e32aff5a46b97b0236
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-23 13:59:19 -0400

    getting current weather using OpenWeather API and Geolocation API

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Calling Unsplash API
 // Getting background image & author name
+let lon;
+let lat;
 
 fetch("https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=RYD8BDXw36XxEEc0URIbaQgqlXvKGmM7gRqmB9fzG3E")
     .then(rsp => rsp.json())
@@ -24,7 +26,6 @@ fetch("https://api.unsplash.com/photos/random?orientation=landscape&query=nature
 fetch("https://api.coingecko.com/api/v3/coins/crypto-com-chain")
     .then(rsp => rsp.json())
     .then(data => {
-        console.log(data);
         const name = data.name;
         const image = data.image.small;
         const price = data.market_data.current_price.usd;
@@ -40,10 +41,30 @@ fetch("https://api.coingecko.com/api/v3/coins/crypto-com-chain")
     })
     .catch(err => console.log(err))
 
+// OpenWeather API
+function callOpenWeatherApi() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=fc739d09b5a3f1f5df587293a34fc6e6`)
+        .then(rsp => {
+            if (!rsp.ok) {
+                throw error('Weather data not available');
+            }
+            return rsp.json();
+        })
+        .then(data => console.log(data))
+        .catch(err => console.error(err))
+    }
+
+// Calling OpenWeather API using latitude & longitude from Geolocation API
+navigator.geolocation.getCurrentPosition((position) => {
+    lon = position.coords.longitude;
+    lat = position.coords.latitude;
+    callOpenWeatherApi()
+  });
+
 
 // Updating time on screen every minute
 function displayTime() {
-    const currentTime = new Date().toLocaleTimeString('en-US', {timeStyle: "medium"});
+    const currentTime = new Date().toLocaleTimeString('en-US', {timeStyle: "short"});
     document.querySelector(".time").textContent = currentTime;
 }
 
